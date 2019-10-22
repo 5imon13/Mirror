5588838ad2242407a82abd6e1cbaf73fead9393c
5imon13 <s19920513@gmail.com>
2019-10-22 09:21:35 +0800

    DB working
    style model added

--- upload/views - copy.py
+++ upload/views - copy.py
@@ -38,6 +38,36 @@ def index(request):
             user = request.session['uid']
             return render(request,"upload/upload.html",locals())
 
+        
+    # elif request.method == "POST":
+    #     f = request.FILES["imageUpload"]
+    #     with open(os.path.join(settings.MEDIA_ROOT, "test.jpg"), "wb+") as destination:
+    #         for chunk in f.chunks():
+    #             destination.write(chunk)
+    #         path = os.path.join(settings.MEDIA_URL, "test.jpg").replace("\\", "/")
+    #     print(f"path: {path}")
+    #     image = Image.open("." + path)
+    #     if image.mode == "P":
+    #         image = image.convert('RGB')
+    #     print("呼叫YOLO，開始辨識")
+    #     yolo = YOLO()
+    #     yolo.clear_session() 
+    #     r_image, result = yolo.detect_image(image)
+    #     print("辨識完成，輸出檔案")
+    #     yolo.clear_session()
+    #     # r_image.show()
+    #     path = path.replace("test.jpg", "out.jpg")
+    #     r_image.save(path[1:])
+    #     print(result)
+    #     if len(result) != 1:
+    #         top = result[1]
+    #         bot = result[0]
+    #     else:
+    #         top, bot = result[0], result[0]
+    #     return render(
+    #         request, "upload/result.html", locals()
+    #     )
+# {"path": path, "top": top, "bot": bot}
 @csrf_exempt
 def result(request):
     bot = 'test'
@@ -58,7 +88,9 @@ def result(request):
             return render(request,"upload/result.html",locals())
         
     elif request.method == "POST":
+        # return render(request,"upload/recommend.html")
         request.session['option'] = 'denim'
+        # return redirect("/upload/recommend/")
         f = request.FILES["imageUpload"]
         with open(os.path.join(settings.MEDIA_ROOT, "test.jpg"), "wb+") as destination:
             for chunk in f.chunks():
@@ -87,6 +119,42 @@ def result(request):
             request, "upload/result.html", locals()
         )
 
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self, count):
+        ret, image = self.video.read()
+        # rotated_image = np.rot90(image)
+        ret, jpeg = cv2.imencode(".jpg", image)
+
+        # path = os.path.join(settings.MEDIA_URL, f"tmp{count}.jpg").replace("\\", "/")
+        # if count%3==0:
+        #     cv2.imwrite(path[1:], rotated_image)
+        return jpeg.tobytes()
+
+
+def gen(camera):
+    count = 0
+    while True:
+        frame = camera.get_frame(count)
+        count+=1
+        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
+
+
+@gzip.gzip_page
+def livefeed(request):
+    try:
+        return StreamingHttpResponse(
+            gen(VideoCamera()), content_type="multipart/x-mixed-replace;boundary=frame"
+        )
+    except HttpResponseServerError as e:
+        print("aborted")
+
+
 @csrf_exempt
 def login(request):
     if request.method == "GET":
@@ -99,9 +167,14 @@ def login(request):
         path = os.path.join(settings.MEDIA_URL, "head.jpg").replace("\\", "/")
         with open(path[1:], 'wb') as f:
             f.write(imgdata)
+        #print(path)
         name = face.detec()
         request.session['uid'] = name
+        # status = 'login'
+        # user = name
         return redirect("/upload/")
+        # render(request,"upload/upload.html",locals())
+        # return HttpResponse(f"<h1>Hello {request.session['uid']}!</h1>")
 
 def recommend(request):
 
@@ -110,6 +183,10 @@ def recommend(request):
         msg = 'Successfully log out!'
         return HttpResponse(json.dumps(msg))
     else:
+        # for obj in Product.objects.all():
+        #     print(obj.img)
+        # template = loader.get_template("upload/recommend.html")
+        # context={}
         option = request.session['option']
         print(option)
         style = Product.objects.filter(style=option)
@@ -128,3 +205,5 @@ def recommend(request):
             status = 'login'
             user = request.session['uid']
             return render(request,"upload/recommend.html",locals())
+    
+
